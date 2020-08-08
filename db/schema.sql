8672ba94aff2fc785411994d4ba267e1ba517cf1
Devmadia <agathaphan@icloud.com>
2020-08-08 15:05:41 -0500

    establishing database and tables, working through PRIMARY KEY discrepancy

--- db/schema.sql
+++ db/schema.sql
@@ -6,27 +6,30 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS roles;
 
-CREATE TABLE departments (
-    id INTEGER,
+CREATE TABLE departments(
+    id INTEGER NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
-    id INTEGER,
+CREATE TABLE roles(
+    id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(6,4),
+    salary DECIMAL,
     department_id INTEGER,
-    PRIMARY KEY (id).
-    CONSTRAINT fk_departments FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    PRIMARY KEY (id),
+    CONSTRAINT fk_departments FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE employees (
-    id INTEGER PRIMARY KEY,
+-- need to get manager_id to reference employee(id)
+-- on delete for manager_id, set to null?
+CREATE TABLE employees(
+    id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
     PRIMARY KEY (id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id)
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO department (id, name) 
+INSERT INTO departments (id, dept_name) 
 VALUES
     (1, 'Sales'),
     (2, 'Engineering'),
     (3, 'Finance'),
     (4, 'Legal');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id) 
 VALUES
     ('Sales Lead', 95000, 1),
     ('Salesperson', 75000, 1),
@@ -15,7 +15,7 @@ VALUES
     ('Legal Team Lead', 275000, 4),
     ('Lawyer', 100000, 4);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id) 
 VALUES
     ('Aleksander', 'Roshane', 1, 3),
     ('Tobias', 'Kohl', 2, 1),
@@ -27,5 +27,3 @@ VALUES
     ('Ashley', 'Martinez', 8, 2),
     ('Sabine', 'Beals', 9, 1),
     ('Rachel', 'Sanchez', 10, null);
-
-
