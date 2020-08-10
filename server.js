7994c9ecbc875ce7922dc34b4fff4839068b1626
Devmadia <agathaphan@icloud.com>
2020-08-09 21:48:38 -0500

    establishing functions for db

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const cTable = require('console.table');
 
 // imports the connection module
 const connection = require('./db/connect');
+const { indexOf } = require('mysql2/lib/constants/charset_encodings');
 
 const init = () => {
+    
     // creates the ascii banner
     var figlet = require('figlet');
  
@@ -22,20 +24,74 @@ const init = () => {
         }
         console.log(data)
     });
-    // inquirer
-    //         .prompt([
-    //             {
-                   
-    //             }
-    //         ])
-    //         .then((data) => {
-    //             console.log("You've made it this far.") 
-    //         })
-    //         .catch((err) => {
-    //             console.log(err);
-    //         });
+
+    inquirer
+            .prompt([
+                {
+                   type: "list",
+                   name: "action",
+                   message: "What would you like to do?",
+                   choices: [
+                       "View all departments",
+                       "View all roles",
+                       "View all employees",
+                       "Add a departments",
+                       "Add an employees",
+                       "Update employee role",
+                       "Quit"
+                   ],
+                }
+            ])
+            .then(({ action }) => {
+                switch (action) {
+                    //query here for actions
+                    case "View all departments":
+                        departmentView();
+                        break;
+
+                    case "View all roles":
+                        rolesView();
+                        break;
+
+                    case "View all employees":
+                        employeeView();
+                        break;
+
+                    case "Add a departments":
+                        departmentAdd();
+                        break;
+
+                    case "Add an employees":
+                        employeesAdd();
+                        break;
+
+                    case "Update employee role":
+                        employeeUpdate();
+                        break;
+                    
+                    case "Quit":
+                        connection.end();
+                        break;
+                }
+            
+            })
+};
+
+// Department Functions
+const departmentView = () => {
+    info
+        .
+        .then(([ row ]) => {
+            console.table(row);
+            init();
+        })
+        .catch(console.log);
 }
 
+
+// Roles functions
+
+// Employees functions
 
 // function call to initialize program
 init();
