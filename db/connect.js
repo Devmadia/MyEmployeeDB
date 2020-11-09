b428b72b92b2436b9f0ac84da80fa85fb25f4b0a
Devmadia <agathaphan@icloud.com>
2020-11-08 19:43:12 -0600

    installed dotenv, initiated to hide DB information, updated .gitignore for dotenv.

--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   // Your MySQL username
-  user: 'root',
+  user: process.env.DB_USER,
   // Your MySQL password
-  password: '',
-  database: 'employee_trackerDB'
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 connection.connect(err => {
