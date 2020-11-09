fd7df8fe8cfbdb344c173b3654d2ed04ce0b87fc
Devmadia <agathaphan@icloud.com>
2020-11-08 19:31:02 -0600

    revised schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -2,35 +2,29 @@ DROP TABLE IF EXISTS employee_trackerDB;
 CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS departments;
+-- DROP TABLE IF EXISTS employees;
+-- DROP TABLE IF EXISTS roles;
+-- DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments(
-    id INTEGER AUTO_INCREMENT,
-    dept_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles(
-    id INTEGER AUTO_INCREMENT,
+    id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(9,2),
-    department_id INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
+    salary DECIMAL(9,2) NOT NULL,
+    department_id INTEGER UNSIGNED NOT NULL
 );
 
 -- need to get manager_id to reference employee(id)
 -- on delete for manager_id, set to null?
 
 CREATE TABLE employees(
-    id INTEGER AUTO_INCREMENT,
+    id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    role_id INTEGER UNSIGNED NOT NULL,
+    manager_id UNSIGNED
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,13 @@
-INSERT INTO departments (id, dept_name) 
+INSERT INTO departments 
+    (name) 
 VALUES
-    (1, 'Sales'),
-    (2, 'Engineering'),
-    (3, 'Finance'),
-    (4, 'Legal');
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
-INSERT INTO roles (title, salary, department_id) 
+INSERT INTO roles 
+    (title, salary, department_id) 
 VALUES
     ('Sales Lead', 95000, 1),
     ('Salesperson', 75000, 1),
@@ -16,18 +18,21 @@ VALUES
     ('Lawyer', 100000, 4);
 
 -- must maintain uniformity with 'INSERT INTO employees...' because database references for employees table doesn't yet exist to be referenced
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Aleksander', 'Roshane', 1, null);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Tobias', 'Kohl', 2, 1);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Jeremiah', 'Hall', 3, 2);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Dinah', 'Wildman', 4, 3);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Johnathan', 'Harker', 5, null);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Pablo', 'Rabien', 6, 5);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Eum', 'Charles', 7, null);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Ashley', 'Martinez', 1, 1);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Sabine', 'Beals', 2, 3);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Rachel', 'Sanchez', 3, null);
+INSERT INTO employees 
+    (first_name, last_name, role_id, manager_id) 
+VALUES 
+    ('Aleksander', 'Roshane', 1, null);
+    ('Tobias', 'Kohl', 2, 1);
+    ('Jeremiah', 'Hall', 3, 2);
+    ('Dinah', 'Wildman', 4, 3);
+    ('Johnathan', 'Harker', 5, null);
+    ('Pablo', 'Rabien', 6, 5);
+    ('Eum', 'Charles', 7, null);
+    ('Ashley', 'Martinez', 1, 1);
+    ('Sabine', 'Beals', 2, 3);
+    ('Rachel', 'Sanchez', 3, null);
 
--- displays tables
-SELECT * FROM departments;
-SELECT * FROM roles;
-SELECT * FROM employees;
+-- -- displays tables
+-- SELECT * FROM departments;
+-- SELECT * FROM roles;
+-- SELECT * FROM employees;
